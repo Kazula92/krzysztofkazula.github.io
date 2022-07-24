3160acb6e0c150a2e390f99cc5efd7a42f143c39
Krzysztof.Kazula <kkazula92@gmail.com>
2022-07-24 21:26:20 +0200

    Frontend23.1

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
   const apiRoot = 'https://app-kodilla-tasks.herokuapp.com/v1/tasks';
-  const trelloApiRoot = 'http://localhost:8081/v1/trello';
+  const trelloApiRoot = 'https://app-kodilla-tasks.herokuapp.com/v1/trello';
   const datatableRowTemplate = $('[data-datatable-row-template]').children()[0];
   const $tasksContainer = $('[data-tasks-container]');
 
