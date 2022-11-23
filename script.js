23a421c6645968df0efa43c48efa19ef6595bf6b
Krzysztof.Kazula <kkazula92@gmail.com>
2022-11-23 19:58:29 +0100

    commit4

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
 
-  const apiRoot = 'http://localhost:8081/v1/tasks';
-  const trelloApiRoot = 'http://localhost:8081/v1/trello';
+  const apiRoot = 'http://localhost:8083/v1/tasks';
+  const trelloApiRoot = 'http://localhost:8083/v1/trello';
   const datatableRowTemplate = $('[data-datatable-row-template]').children()[0];
   const $tasksContainer = $('[data-tasks-container]');
 
